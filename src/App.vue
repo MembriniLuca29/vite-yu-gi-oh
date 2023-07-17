38bc5357611966f3290c06b477f5dcb19e2652ea
Luca Membrini <130695419+MembriniLuca29@users.noreply.github.com>
2023-07-17 13:02:27 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+  <h1>prova</h1>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style lang="scss" scoped>
+$brand_primary: green;
+
+h1{
+  color: $brand_primary;
+}
+</style>
